7eef9a28f2fbffd149eb37b6cbc3bed90b5bd38d
Mick3460 <73465505+Mick3460@users.noreply.github.com>
2020-10-27 16:42:21 +0100

    Vi har tilføjet 2 constructors, getters og setters for alle variable, lavet et toString, for at vise vores objekter (kontoer) oprettet metoder for at hæve, indsætte og overføre penge.

--- src/Account.java
+++ src/Account.java
@@ -4,6 +4,7 @@ public class Account {
 
     static Scanner scan = new Scanner(System.in);
 
+    double bankingFee = 5;
     private int accountID;
     private String name;
     private double balance;
@@ -14,28 +15,68 @@ public class Account {
         this.balance = balance;
     }
 
+    //Default constructor.
+    Account(){    }
+
     int getAccountID() {return this.accountID;}
 
     double getBalance() {return this.balance;}
 
     String getName() {return this.name;}
 
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void setAccountID(int accountID) {
+        this.accountID = accountID;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
     public String toString() {
         return "accountID = " + getAccountID() + "\nName = " + getName() + "\nBalance = " + getBalance();
     }
 
-    withdrawal(){
+
+    double withdrawal (){
+        System.out.println("Angiv det beløb du vil HÆVE: \n");
+        double customerInput = scan.nextDouble();
+
+        if (customerInput > getBalance() ){
+            System.out.println("Den går ikke makker");
+            return -1;
+        } else {
+        setBalance(getBalance() - customerInput);
+            System.out.println("Du har nu hævet, og din kontobalance er: " + getBalance());
+        return getBalance();
+        }
 
     }
 
-    void deposit(double amount){
-
-
+    void deposit(){
+        System.out.println("Angiv det beløb du vil indsætte: \n");
+        double customerInput = scan.nextDouble();
+        setBalance(getBalance() + customerInput);
+        System.out.println(getBalance());
     }
 
-    transfer(){
+    void transfer(double belob){
 
+        if ((belob +5) > getBalance() ){
+            System.out.println("Den går ikke makker");
+        } else {
+            setBalance(getBalance() - belob - bankingFee);
+            System.out.println("Du har nu overført: " + belob + " og din kontobalance er: " + getBalance());
+            System.out.println("Husk det koster 5kr i gebyrer tiihiii :)");
+
+        }
     }
+
+
 
 
 
--- src/AccountManagement.java
+++ src/AccountManagement.java
@@ -5,8 +5,22 @@ public class AccountManagement {
     static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) {
+
     Account konto = new Account(123,"Gus",500.10);
+
         System.out.println(konto.toString());
+        System.out.println("Tester deposit");
+        konto.deposit();
+
+        System.out.println("Nu tester vi withdrawal");
+        konto.withdrawal();
+
+        System.out.println("Tester TRANSFER\nHvor meget vil du overføre?");
+        double belob = scan.nextDouble();
+        konto.transfer(belob);
+
+
+
 
 
 
